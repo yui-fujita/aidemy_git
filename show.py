d172ebadffe04d09308d1227123fc343611ecedd
yui.fujita <yui.fujita@toppan.co.jp>
2021-10-18 14:54:55 +0900

    add main

--- show.py
+++ show.py
@@ -1,5 +1,9 @@
 import random
 
-with open("./hyakunin.txt",encoding="utf-8") as f:
-	wakas=[s.strip() for s in f.readlines()]
-print(wakas[random.randrange(len(wakas))])
+def main():
+	with open("./hyakunin.txt",encoding="utf-8") as f:
+		wakas=[s.strip() for s in f.readlines()]
+	print(wakas[random.randrange(len(wakas))])
+
+if __name__ == '__main__':
+	main()
